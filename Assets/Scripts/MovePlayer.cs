172ac105020e582a75e5725e5fab6d27ebc2c52f
Hasan Çomak <hsncomak2222@gmail.com>
2026-02-26 16:08:40 +0300

    Project Started

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpSpeed;
+    private Rigidbody2D rb;
+    public bool doubleJump;
+
+    public Transform groundCheck;
+    public float circleRadius = 0.2f;
+    public LayerMask _layerMask;
+    float posX;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded())
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+                doubleJump = true;
+            }
+            else if (doubleJump)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+                doubleJump= false;
+            }
+        }
+        flip();
+    }
+    private void FixedUpdate()
+    {
+        posX = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector3(posX * speed, rb.linearVelocity.y, 0f);
+    }
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, circleRadius, _layerMask) != null;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, circleRadius);
+    }
+    void flip()
+    {
+        if (posX > 0.01f) transform.localScale = new Vector3(1, 1, 1);
+        if (posX < -0.01f) transform.localScale = new Vector3(-1, 1, 1);
+    }
+}
